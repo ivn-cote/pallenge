edebf42551387411e4b0d32596a8192a44e0a1d0
Ivan Kotov <ivn@dreamindustries.co>
2014-10-25 23:22:15 +0400

    some style

--- assets/js/src/app.js
+++ assets/js/src/app.js
@@ -1,8 +1,15 @@
 /** @jsx React.DOM */
-var bemMixin = require('./bem');
+var bemMixin = require('./bem'),
+  lib = {};
 
-var AppComponent = React.createClass({
+lib.App = React.createClass({
   mixins: [bemMixin],
+  getInitialState: function() {
+    $.get('/user/1/tasks/').then(this.onReceiveCards);
+    return {
+      initCards: []
+    }
+  },
   getDefaultProps: function() {
     return {
       menuItems: [
@@ -29,6 +36,16 @@ var AppComponent = React.createClass({
       ]
     }
   },
+  onReceiveCards: function(res) {
+    var cards = res.map(function(card) {
+      return {
+        from: card['src_user']['Name'],
+        title: card['challenge']['Name'],
+        description: 'TODO'
+      }
+    });
+    this.setState({ initCards: cards });
+  },
   render: function() {
     var menuLines = this.props.menuItems.map(function(line) {
       return (
@@ -41,12 +58,17 @@ var AppComponent = React.createClass({
       );
     });
 
+    var initCardsNodes = this.state.initCards.map(function(card) {
+      return (
+        <lib.ChallengeCard data={card} />
+      );
+    });
+
     return (
       <div className={this.b_()}>
-        <div className="page-header">
-          <h1>PALLENGE</h1>
-        </div>
-        <ul className="nav nav-pills nav-stacked">
+        <lib.Header />
+        {initCardsNodes}
+        <ul className={'nav nav-pills nav-stacked ' + this.b_('-menu')}>
           {menuLines}
         </ul>
       </div>
@@ -54,7 +76,44 @@ var AppComponent = React.createClass({
   }
 });
 
+lib.Header = React.createClass({
+  mixins: [bemMixin],
+  render: function() {
+    return (
+      <div className={this.b_()}>
+        <div className={this.b_('-logo')}/>
+      </div>
+    );
+  }
+});
+
+
+lib.ChallengeCard = React.createClass({
+  mixins: [bemMixin],
+  getDefaultProps: function() {
+    return {
+      data: {
+        from: 'Odin',
+        description: 'Turn the world',
+        title: 'Hard night'
+      }
+    }
+  },
+  render: function() {
+    return (
+      <div className={this.b_()}>
+        <div className={this.b_('-img')}/>
+        <div className={this.b_('-info')}>
+          <div className={this.b_('-title')}>{this.props.data.title}</div>
+          <div className={this.b_('-user')}>{this.props.data.from}</div>
+          <div>{this.props.data.description}</div>
+        </div>
+      </div>
+    );
+  }
+});
+
 React.renderComponent(
-  <AppComponent />,
+  <lib.App />,
   document.getElementById('app')
 );
