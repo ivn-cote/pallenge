43bbd33394e3d23eda4081cd68a37ad842bbbe6e
Ivan Kotov <ivn@dreamindustries.co>
2014-10-25 21:06:16 +0400

    draft of main page

--- assets/js/src/app.js
+++ assets/js/src/app.js
@@ -3,10 +3,52 @@ var bemMixin = require('./bem');
 
 var AppComponent = React.createClass({
   mixins: [bemMixin],
+  getDefaultProps: function() {
+    return {
+      menuItems: [
+        {
+          title: 'Full todo list',
+          count: 3
+        },
+        {
+          title: 'Need to be paid',
+          count: 4
+        },
+        {
+          title: 'Sent',
+          count: 4
+        },
+        {
+          title: 'Popular',
+          count: 4
+        },
+        {
+          title: 'Completed',
+          count: 3
+        }
+      ]
+    }
+  },
   render: function() {
+    var menuLines = this.props.menuItems.map(function(line) {
+      return (
+        <li className="active">
+          <a href="#">
+            <span className="badge pull-right">{line.count}</span>
+            {line.title}
+          </a>
+        </li>
+      );
+    });
+
     return (
-      <div className={this.b_('-aa')}>
-        Hello, world! I am an APP
+      <div className={this.b_()}>
+        <div className="page-header">
+          <h1>PALLENGE</h1>
+        </div>
+        <ul className="nav nav-pills nav-stacked">
+          {menuLines}
+        </ul>
       </div>
     );
   }
